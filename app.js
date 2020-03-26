f316c76cab7fc2c54ed7f8c8b47d2e5a3bfa0c1a
IshTakkar <ishtakkar21@gmail.com>
2020-03-26 14:11:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose')
+    request = require('request');
+
+
+//APP CONFIGS
+mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useUnifiedTopology: true, useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+//MONGOOSE CONFIGS
+var blogSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blog) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { blogs: blog });   
+        }
+    });
+});
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Port opened on 3000");
+});
