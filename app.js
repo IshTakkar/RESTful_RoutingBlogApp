a8552669cae80e750438b26946912a3c74f11387
IshTakkar <ishtakkar21@gmail.com>
2020-03-31 21:00:02 +0530

    added update and delete

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose')
+    mongoose = require('mongoose'),
+    methodOverride = require('method-override'),
     request = require('request');
 
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useUnifiedTopol
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 
 //MONGOOSE CONFIGS
@@ -59,6 +61,36 @@ app.get("/blogs/:id", (req, res) => {
             console.log("found error");
         } else {
             res.render("show", { blog: foundBlog });
+        }
+    });
+});
+
+app.get("/blogs/:id/edit", (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) {
+            console.log("Error found.");
+        } else {
+            res.render("edits", { blog: foundBlog });
+        }
+    });
+});
+
+app.put("/blogs/:id", (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blogs, (err, updatedBlog) => {
+        if (err) {
+            console.log("Error found");
+        } else{
+            res.redirect("/blogs/" + req.params.id);
+       }
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, (err, deletedBlog) => {
+        if (err) {
+            console.log("Error found");
+        } else {
+            res.redirect("/blogs");
         }
     });
 });
